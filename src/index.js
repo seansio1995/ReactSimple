56645c2d299d8bb425aa99e0ff06ab2a759b9be7
seansio1995 <seansio@163.com>
2018-07-16 02:53:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -14,16 +15,23 @@ class App extends Component{
   constructor(props){
     super(props);
 
-    this.state={videos:[]};
+    this.state={videos:[],
+    selectedVideo:null};
     YTSearch({key:API_KEY,term:'surfboards'},(videos)=>{
-      this.setState({videos:videos});
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+      });
     });
   }
   render(){
   return (
     <div>
       <SearchBar/>
-      <VideoList videos={this.state.videos}/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+      onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+      videos={this.state.videos}/>
     </div>
   );
 }
